19fda1474c52de7baca41e7531e500898c89564a
Nir Sela <33286680+NirSe@users.noreply.github.com>
2017-12-13 18:45:24 +0200

    Script to remove all data from drivers, passengers and cells

---
+++ taxi_nosql/remove_all_data.sh
@@ -0,0 +1,6 @@
+hdfs dfs -rm -r /taxi_example/passengers
+hdfs dfs -rm -r /taxi_example/drivers
+hdfs dfs -rm -r /taxi_example/cells
+hdfs dfs -mkdir /taxi_example/passengers
+hdfs dfs -mkdir /taxi_example/drivers
+hdfs dfs -mkdir /taxi_example/cells
