17b77b774890e15a9ba62b74d1c17b7c59d1c1bf
Nir Sela <33286680+NirSe@users.noreply.github.com>
2018-01-15 12:54:42 +0200

    Update create_taxi_stream.sh

--- taxi_streaming/create_taxi_stream.sh
+++ taxi_streaming/create_taxi_stream.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-NGINX_IP='127.0.0.1'
+NGINX_IP="127.0.0.1"
+NGINX_PORT="8081"
+CONTAINER_ID=1
+STREAM_PATH="taxi_example/driver_stream"
 
-STREAM_NAME=taxi_example/driver_stream
-
-curl  -X PUT \
-     --header "Content-type: application/json" \
-     --header "X-v3io-function: CreateStream" \
-     --header "Cache-Control: no-cache" \
-     --data '{"ShardCount": 12, "RetentionPeriodHours": 1}' \
-     http://127.0.0.1:8081/1/$STREAM_NAME/
-
+curl -X PUT \
+    --header "Content-type: application/json" \
+    --header "X-v3io-function: CreateStream" \
+    --header "Cache-Control: no-cache" \
+    --data '{"ShardCount": 12, "RetentionPeriodHours": 1}' \
+    http://${NGINX_IP}:${NGINX_PORT}/${CONTAINER_ID}/${STREAM_PATH}/
