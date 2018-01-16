eb3bdfe6f50896826b93f9d22bedaffcbe7042d2
Nir Sela <nirse@iguazio.com>
2018-01-16 22:38:42 +0200

    rename taxi to drivers

--- taxi_streaming/spark-submit_cmd.sh
+++ taxi_streaming/spark-submit_cmd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Submit a spark job to consume the stream data
-spark-submit --master yarn --py-files ~/igz/bigdata/libs/v3io-py.zip consume_taxi_to_kv.py
+spark-submit --master yarn --py-files ~/igz/bigdata/libs/v3io-py.zip consume_drivers_stream_data.py
 
