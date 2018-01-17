6dad64be5dcd6f97365f14fb9448cfa171f24736
Sharon Lifshitz <sharonl@iguaz.io>
2018-01-17 18:28:40 +0200

    stream_drivers_data.var input-file variable renaming for consistency

--- taxi_streaming/stream_drivers_data.sh
+++ taxi_streaming/stream_drivers_data.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Stream-data input file
-STREAM_DATA_INPUT_FILE="drivers_data.csv"
+INPUT_FILE="drivers_data.csv"
 
 # Add data to the stream and ingest it into the platform
-python stream_drivers_data.py ${STREAM_DATA_INPUT_FILE}
+python stream_drivers_data.py ${INPUT_FILE}
 
