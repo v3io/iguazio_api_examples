cefec9827f04b6b3532646ff6f0c946732c0af4c
Sharon Lifshitz <sharonl@iguaz.io>
2018-01-17 11:34:25 +0200

    taxi_streaming example files renaming #3
    MERGE commit eb3bdfe from NiRSE.master (PR #6)

--- taxi_streaming/stream_drivers_data.sh
+++ taxi_streaming/stream_drivers_data.sh
@@ -4,5 +4,5 @@
 STREAM_DATA_INPUT_FILE="drivers_data.csv"
 
 # Add data to the stream and ingest it into the platform
-python stream_driver_data.py ${STREAM_DATA_INPUT_FILE}
+python stream_drivers_data.py ${STREAM_DATA_INPUT_FILE}
 
