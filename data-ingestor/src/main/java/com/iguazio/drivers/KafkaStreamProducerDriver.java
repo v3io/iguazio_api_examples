0239e1eb3039ec881cbaa5d1365caafde6764e44
anant-g <anantg@192.168.0.128>
2018-03-02 16:22:40 +0800

    test

--- data-ingestor/src/main/java/com/iguazio/drivers/KafkaStreamProducerDriver.java
+++ data-ingestor/src/main/java/com/iguazio/drivers/KafkaStreamProducerDriver.java
@@ -22,7 +22,7 @@ public class KafkaStreamProducerDriver {
 
 	public void init() {
 
-		props.put("bootstrap.servers", "kafka:9092");
+		props.put("bootstrap.servers", "localhost:9092");
 		props.put("acks", "all");
 		props.put("retries", 0);
 		props.put("batch.size", 1500);
