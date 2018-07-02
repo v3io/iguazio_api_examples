bce3b583f076c9203911875b1fd94dc441df05c8
Nir Sela <33286680+NirSe@users.noreply.github.com>
2018-07-02 08:49:59 +0300

    Support for Passengers
    Support for RecordType in ingest, enabling ingestion of both passengers and drivers

--- taxi_nosql/nuclio_python/generate_data.py
+++ taxi_nosql/nuclio_python/generate_data.py
@@ -6,7 +6,10 @@ from random import *
 
 def handler(context, event):
 
-    # list of random locations 
+    # Get IP and Port of the ingestion function from environment variables defined using Nuclio
+    INGEST_URL = os.getenv ("INGEST_URL")
+    
+    # List of random locations - feel free to replace the list with your favourite locations
     Downtown_London = [-0.1195,51.5033]
     Westminster = [-0.1357,51.4975]
     Oxford_St = [-0.1410,51.5154]
@@ -16,8 +19,10 @@ def handler(context, event):
     Canary_Wharf = [-0.0235,51.5054]
     Locations = [Canary_Wharf,Canary_Wharf,Downtown_London,Westminster,Oxford_St,Oxford_St,Oxford_St,Heathrow,Heathrow_Parking,Gatwick]
 
+    # Get IP and Port of the ingestion function from environment variables defined using Nuclio
     INGEST_URL = os.getenv ("INGEST_URL")
 
+    # Create a session for sending requests for ingestion
     s = requests.Session()
     request_json = {}
 
@@ -31,22 +36,52 @@ def handler(context, event):
         longitude =  Decimal (Locations[rnd_location][0]) + (Decimal (rnd_long) / Decimal (300))
         latitude = Decimal (Locations[rnd_location][1])  + (Decimal (rnd_lat) / Decimal (300))
         
-        context.logger.info('driver id - '+ str(rnd_driver))
+        #context.logger.info('driver id - '+ str(rnd_driver))
 
-        request_json["driverID"] = rnd_driver
+        # Build request json
+        request_json["RecordType"] = "driver"
+        request_json["ID"] = rnd_driver
         request_json["longitude"] = str (longitude)
         request_json["latitude"] = str (latitude)
         
         payload = json.dumps(request_json)
 
+        # send request to ingest functiom
         res = s.put(INGEST_URL, data=payload)
         
-        context.logger.info(request_json)
-        context.logger.info(res)
+        #context.logger.info(request_json)
+        #context.logger.info(res)
+
+   # generate random location for 500 passengers and send for ingestion
+    for x in range (1,500):
+        rnd_driver = randint (1,5000)
+        rnd_location = randint (0,9)
+        rnd_radius =randint (0,8)
+        rnd_long = randint (-rnd_radius,rnd_radius)
+        rnd_lat  = randint (-rnd_radius,rnd_radius)
+        longitude =  Decimal (Locations[rnd_location][0]) + (Decimal (rnd_long) / Decimal (300))
+        latitude = Decimal (Locations[rnd_location][1])  + (Decimal (rnd_lat) / Decimal (300))
+        
+        #context.logger.info('passenger id - '+ str(rnd_driver))
+
+        # Build request json
+        request_json["RecordType"] = "passenger"
+        request_json["ID"] = rnd_driver
+        request_json["longitude"] = str (longitude)
+        request_json["latitude"] = str (latitude)
+        
+        payload = json.dumps(request_json)
+
+        # send request to ingest functiom
+        res = s.put(INGEST_URL, data=payload)
+        
+        
+        #context.logger.info(request_json)
+        #context.logger.info(res)
 
     context.logger.info('End - Generating Data')
 
-    return context.Response(body='Ingestion of 1000 drivers completed successfully',
+    return context.Response(body='Ingestion of drivers and passengers completed successfully',
                             headers={},
                             content_type='text/plain',
                             status_code=200)
--- taxi_nosql/nuclio_python/ingest.py
+++ taxi_nosql/nuclio_python/ingest.py
@@ -1,134 +1,1 @@
-import requests
-import json
-import os
-import s2sphere
-
-WEBAPI_URL = os.getenv('WEBAPI_URL')
-
-DRIVERS_PATH_IN_URL = os.getenv('DRIVERS_TABLE')
-CELLS_PATH_IN_URL   = os.getenv('CELLS_TABLE')
-
-V3IO_HEADER_FUNCTION = 'X-v3io-function'
-
-def handler(context, event):
-
-    input_data_json = event.body 
-
-    input_data = json.loads(input_data_json)
-
-    driver_id = str(input_data ["driverID"])
-
-    longitude = float (input_data["longitude"])
-    latitude = float (input_data["latitude"])
-
-    # Use google s2Sphere library to calculate Cell 
-    p1 = s2sphere.LatLng.from_degrees(latitude,longitude)
-    cell = s2sphere.CellId.from_lat_lng(p1).parent(15)
-    cell_id = str(cell.id())
-    
-    s = requests.Session()
-
-    # update driver current and previous location 
-    res = ngx_update_expression_request(s,WEBAPI_URL, DRIVERS_PATH_IN_URL + "driver_" + driver_id, None, None,
-                                        "CreateOrReplaceAttributes",
-                                        "previous_cell_id=current_cell_id;current_cell_id=" + cell_id + ";change_cell_id_indicator=(previous_cell_id != current_cell_id);",
-                                        "exists(current_cell_id)")
-    
-    if res.status_code == requests.codes.bad_request:
-            ngx_update_expression_request(s,WEBAPI_URL, DRIVERS_PATH_IN_URL + "driver_" + driver_id, None, None,
-                                          "CreateOrReplaceAttributes",
-                                          "current_cell_id=" + cell_id + ";previous_cell_id=0;change_cell_id_indicator=(1==1);",
-                                          "(1==1)")
-
-    # Get current and previous cell for driver 
-    response_json = ngx_get_item_request(s,WEBAPI_URL, DRIVERS_PATH_IN_URL+"driver_"+driver_id,None,None,exp_attrs=["change_cell_id_indicator","current_cell_id","previous_cell_id"])
-
-    # Check if a cell update is needed
-    attrs = response_json["Item"]
-    change_cell_id_indicator_val = attrs["change_cell_id_indicator"]["BOOL"]
-    current_cell_id_val = attrs["current_cell_id"]["N"]
-    previous_cell_id_val = attrs["previous_cell_id"]["N"]
-    #for key, value in attrs.items():
-    #     print("attr: {}, value: {}".format(key, value["N"]))
-
-    # Try to increase the count on the cell the driver moved to
-    if change_cell_id_indicator_val:
-        res= ngx_update_expression_request(s,WEBAPI_URL, CELLS_PATH_IN_URL + "cell_"+ current_cell_id_val, None, None,
-                                          "CreateOrReplaceAttributes",
-                                          "count=count+1;",
-                                          "exists(count)")
-        # If the cell doesn't exists create a new one
-        if res.status_code == requests.codes.bad_request:
-                ngx_update_expression_request(s,WEBAPI_URL, CELLS_PATH_IN_URL + "cell_" + current_cell_id_val, None, None,
-                                              "CreateOrReplaceAttributes",
-                                              "count=1;",
-                                              None)
-        # Decrease the count on the cell the driver moved from
-        ngx_update_expression_request(s,WEBAPI_URL, CELLS_PATH_IN_URL + "cell_" + previous_cell_id_val, None, None,
-                                          "CreateOrReplaceAttributes",
-                                          "count=count-1;;",
-                                          None)
-
-    return context.Response(body='Ingestion completed successfully',
-                            headers={},
-                            content_type='text/plain',
-                            status_code=200)
-
-def ngx_get_item_request(
-        s,base_url, path_in_url, table_name=None, key=None, exp_attrs=None, expected_result=requests.codes.ok):
-    url = base_url + path_in_url
-
-    request_json = {}
-
-    if table_name is not None:
-        request_json["TableName"] = table_name
-
-    request_json["AttributesToGet"] = ""
-
-    for attr_name in exp_attrs:
-        if request_json["AttributesToGet"] != "":
-            request_json["AttributesToGet"] += ","
-        request_json["AttributesToGet"] += attr_name
-
-    payload = json.dumps(request_json)
-    headers = {V3IO_HEADER_FUNCTION: 'GetItem'}
-    res = s.put(url, data=payload, headers=headers)
-
-    #print(res.content)
-    assert res.status_code == expected_result
-    if expected_result != requests.codes.ok:
-        return
-
-    response_json = json.loads(res.content)
-    return response_json
-
-#
-# Construct and send UpdateItem web reuest
-#
-def ngx_update_expression_request(
-        s,base_url, path_in_url, table_name=None, key=None, mode=None, update_expr=None, text_filter=None, type="UpdateItem",
-        expected_result=requests.codes.no_content):
-    url = base_url + path_in_url
-
-    request_json = {}
-
-    if table_name is not None:
-        request_json["TableName"] = table_name
-
-    if mode is not None:
-        request_json["UpdateMode"] = mode
-
-    if update_expr is not None:
-        request_json["UpdateExpression"] = update_expr
-
-    if text_filter is not None:
-        request_json["ConditionExpression"] = text_filter
-
-    payload = json.dumps(request_json)
-    headers = {V3IO_HEADER_FUNCTION: type}
-    #print(payload)
-    res = s.put(url, data=payload, headers=headers)
-    #print(res)
-    #assert res.status_code == expected_result
-    return res
-
+import requestsimport jsonimport osimport s2sphere# Get configuration information from environment variables defined using NuclioWEBAPI_URL     = str(os.getenv('WEBAPI_URL'))CONTAINER_NAME = str(os.getenv('CONTAINER_NAME'))# Get table locations from environment variables defined using NuclioDRIVERS_TABLE_PATH = CONTAINER_NAME + str(os.getenv('DRIVERS_TABLE'))PASSENGERS_TABLE_PATH = CONTAINER_NAME + str(os.getenv('PASSENGERS_TABLE'))CELLS_TABLE_PATH   = CONTAINER_NAME + str(os.getenv('CELLS_TABLE'))DRIVER_PREFIX = 'driver_'PASSENGER_PREFIX = 'passenger_'V3IO_HEADER_FUNCTION = 'X-v3io-function'def handler(context, event):    # Read the data to be ingested from the input json    input_data_json = event.body     input_data = json.loads(input_data_json)    record_type = str(input_data ["RecordType"])    id = str(input_data ["ID"])        longitude = float (input_data["longitude"])    latitude = float (input_data["latitude"])    # Use google s2Sphere library to calculate Cell from longitude and latitude    p1 = s2sphere.LatLng.from_degrees(latitude,longitude)    cell = s2sphere.CellId.from_lat_lng(p1).parent(15)    cell_id = str(cell.id())        # Create a session for sending NoSQL Webapi requests    s = requests.Session()    # Ingestion of both drivers and passengers are supported    # Depening on record type, releavant tables will be updated    if record_type == 'driver':        ITEM_PREFIX = DRIVER_PREFIX        ITEM_PATH   = DRIVERS_TABLE_PATH    else :        ITEM_PREFIX = PASSENGER_PREFIX        ITEM_PATH = PASSENGERS_TABLE_PATH    # update driver current and previous location     res = ngx_update_expression_request(s,WEBAPI_URL, ITEM_PATH + ITEM_PREFIX +id, None, None,                                            None,                                            "SET previous_cell_id=if_not_exists(current_cell_id,0);current_cell_id=" + cell_id + ";change_cell_id_indicator=(previous_cell_id != current_cell_id);",                                            None)    # context.logger.info(res)        # Get current and previous cell for driver     response_json = ngx_get_item_request(s,WEBAPI_URL, ITEM_PATH + ITEM_PREFIX +id,None,None,exp_attrs=["change_cell_id_indicator","current_cell_id","previous_cell_id"])    # Check if a cell update is needed    attrs = response_json["Item"]    change_cell_id_indicator_val = attrs["change_cell_id_indicator"]["BOOL"]    current_cell_id_val = attrs["current_cell_id"]["N"]    previous_cell_id_val = attrs["previous_cell_id"]["N"]    # if cell was changed, increase the count on the new cell and descrease from the old cell    if change_cell_id_indicator_val:            # Increase the count on the currnet cell            res=ngx_update_expression_request(s,WEBAPI_URL, CELLS_TABLE_PATH + "cell_"+ current_cell_id_val, None, None,                                          None,                                          "SET "+ITEM_PREFIX+"count=if_not_exists("+ITEM_PREFIX+"count,0)+1;",                                          None)            # context.logger.info(res)            # Decrease the count on the previous cell            res = ngx_update_expression_request(s,WEBAPI_URL, CELLS_TABLE_PATH + "cell_" + previous_cell_id_val, None, None,None,                                          "SET "+ITEM_PREFIX+"count="+ITEM_PREFIX+"count-1;",                                          None)            # context.logger.info(res)    return context.Response(body='Ingestion completed successfully',                            headers={},                            content_type='text/plain',                            status_code=200)## Construct and send GetItem NoSQL Web API reuest#def ngx_get_item_request(        s,base_url, path_in_url, table_name=None, key=None, exp_attrs=None, expected_result=requests.codes.ok):    url = base_url + path_in_url    #    # Construct the json         #    request_json = {}    if table_name is not None:        request_json["TableName"] = table_name    request_json["AttributesToGet"] = ""    for attr_name in exp_attrs:        if request_json["AttributesToGet"] != "":            request_json["AttributesToGet"] += ","        request_json["AttributesToGet"] += attr_name    payload = json.dumps(request_json)    headers = {V3IO_HEADER_FUNCTION: 'GetItem'}    # send the request    res = s.put(url, data=payload, headers=headers)    assert res.status_code == expected_result    if expected_result != requests.codes.ok:        return    response_json = json.loads(res.content)    return response_json## Construct and send UpdateItem NoSQL Web API reuest#def ngx_update_expression_request(        s,base_url, path_in_url, table_name=None, key=None, mode=None, update_expr=None, text_filter=None, type="UpdateItem",        expected_result=requests.codes.no_content):    url = base_url + path_in_url    #    # Construct the json         #    request_json = {}    if table_name is not None:        request_json["TableName"] = table_name    if mode is not None:        request_json["UpdateMode"] = mode    if update_expr is not None:        request_json["UpdateExpression"] = update_expr    if text_filter is not None:        request_json["ConditionExpression"] = text_filter    payload = json.dumps(request_json)    headers = {V3IO_HEADER_FUNCTION: type}     # send the request    res = s.put(url, data=payload, headers=headers)    #assert res.status_code == expected_result    return res
