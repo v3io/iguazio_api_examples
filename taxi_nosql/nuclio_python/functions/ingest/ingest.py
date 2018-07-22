2a3ed8f144d63f700c6c50085f204ddb0b1f728e
Nir Sela <33286680+NirSe@users.noreply.github.com>
2018-07-22 18:39:13 +0300

    Broke into sub functions to make it readable

--- taxi_nosql/nuclio_python/functions/ingest/ingest.py
+++ taxi_nosql/nuclio_python/functions/ingest/ingest.py
@@ -4,118 +4,137 @@ import os
 import s2sphere
 
 # Get configuration information from environment variables defined using Nuclio
-# Web-APIs (web-gateway) URL
-WEBAPI_URL     = str(os.getenv('WEBAPI_URL'))
-# Container name
+WEBAPI_URL = str(os.getenv('WEBAPI_URL'))
 CONTAINER_NAME = str(os.getenv('CONTAINER_NAME'))
-# Web-APIs user credentials (used for HTTP authentication)
-WEBAPI_USER = os.getenv('WEBAPI_USER')
-WEBAPI_PASSWORD = os.getenv('WEBAPI_PASSWORD')
+
 WEBAPI_CRED = os.getenv('WEBAPI_CRED')
-# Table paths
+
+# Get table locations from environment variables defined using Nuclio
 DRIVERS_TABLE_PATH = CONTAINER_NAME + str(os.getenv('DRIVERS_TABLE'))
 PASSENGERS_TABLE_PATH = CONTAINER_NAME + str(os.getenv('PASSENGERS_TABLE'))
-CELLS_TABLE_PATH   = CONTAINER_NAME + str(os.getenv('CELLS_TABLE'))
+CELLS_TABLE_PATH = CONTAINER_NAME + str(os.getenv('CELLS_TABLE'))
 
 DRIVER_PREFIX = 'drivers_'
 PASSENGER_PREFIX = 'passengers_'
 
 V3IO_HEADER_FUNCTION = 'X-v3io-function'
 
+
+#
+# Ingest data into driver/passenger and cells tables
+# 
 def handler(context, event):
 
-    # Read the data to be ingested from the input JSON request body
-    input_data_json = event.body 
+    # Generate the input data based on input from event.body
+    id, cell_id, item_prefix = _generate_data_from_input(event.body)
+
+    # Update driver/passenger current and previous location
+    res = _webapi_updateitem(WEBAPI_URL,
+                             id,
+                             f'SET previous_cell_id=if_not_exists(current_cell_id,0);current_cell_id={cell_id};change_cell_id_indicator=(previous_cell_id != current_cell_id);')
+    context.logger.error(f'Error during update of {WEBAPI_URL}{id}. Error code is {res.status_code}')
+
+    if res.status_code != requests.codes.no_content:
+            context.logger.error(f'Error during update of {WEBAPI_URL}{id}. Error code is {res.status_code}')
+            return context.Response(body='Internal error during ingestion', content_type='text/plain', status_code=500)
+
+    # Update cells table according to driver/passenger current and previous location
+    res = _update_cells_table(context, id, item_prefix)
+    if res.status_code != requests.codes.no_content:
+            context.logger.error(f'Error during update of cells table. Error code is {res.status_code}')
+            return context.Response(body='Internal error during ingestion', content_type='text/plain', status_code=500)
+
+    # return 200 - completed successfuly
+    return context.Response(status_code=200)
+
+
+#
+# Generate data from input - parse the json and analyze
+#
+def _generate_data_from_input(input_data_json):
+
+    # Read the data to be ingested from the input json
     input_data = json.loads(input_data_json)
 
-    record_type = str(input_data ["RecordType"])
+    record_type = str(input_data["RecordType"])
 
-    id = str(input_data ["ID"])
-    
-    longitude = float (input_data["Longitude"])
-    latitude = float (input_data["Latitude"])
+    input_id = str(input_data["ID"])
 
-    # Use the Google s2sphere library to calculate a cell from the longitude
-    # and latitude degrees
-    p1 = s2sphere.LatLng.from_degrees(latitude,longitude)
+    longitude = float(input_data["Longitude"])
+    latitude = float(input_data["Latitude"])
+
+    # Use google s2Sphere library to calculate Cell from longitude and latitude
+    p1 = s2sphere.LatLng.from_degrees(latitude, longitude)
     cell = s2sphere.CellId.from_lat_lng(p1).parent(15)
     cell_id = str(cell.id())
-    
-    # Create a session for sending NoSQL Web API requests
-    s = requests.Session()
 
-    # Provide webapi user/pass
-    if WEBAPI_USER is not None:
-        s.auth = (str(WEBAPI_USER), str(WEBAPI_PASSWORD))
-    
     # Ingestion of both drivers and passengers are supported
-    # Depending on record type, relevant tables will be updated
+    # Depening on record type, releavant tables will be updated
     if record_type == 'driver':
-        ITEM_PREFIX = DRIVER_PREFIX
-        ITEM_PATH   = DRIVERS_TABLE_PATH
-    else :
-        ITEM_PREFIX = PASSENGER_PREFIX
-        ITEM_PATH = PASSENGERS_TABLE_PATH
+        item_prefix = DRIVER_PREFIX
+        item_path = DRIVERS_TABLE_PATH
+    else:
+        item_prefix = PASSENGER_PREFIX
+        item_path = PASSENGERS_TABLE_PATH
 
-    # Update the driver's current and previous location 
-    res = ngx_update_expression_request(s,WEBAPI_URL, ITEM_PATH + ITEM_PREFIX +id, None, None,
-                                            None,
-                                            "SET previous_cell_id=if_not_exists(current_cell_id,0);current_cell_id=" + cell_id + ";change_cell_id_indicator=(previous_cell_id != current_cell_id);",
-                                            None)
+    id = item_path + item_prefix + input_id
 
-    if res.status_code not in (200,204):
-            context.logger.error ("Error during update of drivers table. Error code is "+str(res.status_code))
-            return context.Response(body='Internal error during ingestion',content_type='text/plain',status_code=500)
-    
-    # Get the driver's current and previous cell
-    response_json = ngx_get_item_request(s,WEBAPI_URL, ITEM_PATH + ITEM_PREFIX +id,None,None,exp_attrs=["change_cell_id_indicator","current_cell_id","previous_cell_id"])
+    return id, cell_id, item_prefix
 
-    # Check whether a cell-update is needed
+
+#
+# If Cell(location) was changed, update cells table based on new location
+# Increase the count on the new cell, decrease the count on the old cell if needed
+#
+def _update_cells_table(context, id, item_prefix):
+
+    # Get current and previous cell for driver
+    response_json = _webapi_getitem(WEBAPI_URL, id, exp_attrs=["change_cell_id_indicator", "current_cell_id", "previous_cell_id"])
+
+    # Check if a cell update is needed
     attrs = response_json["Item"]
     change_cell_id_indicator_val = attrs["change_cell_id_indicator"]["BOOL"]
     current_cell_id_val = attrs["current_cell_id"]["N"]
     previous_cell_id_val = attrs["previous_cell_id"]["N"]
 
-    # If the driver's cell has changed, increase the drivers count for the new
-    # cell and decrease the count for the old cell
+    # if cell was changed, increase the count on the new cell and descrease from the old cell
     if change_cell_id_indicator_val:
-            # Increase the count for the driver's new (current) cell
-            res=ngx_update_expression_request(s,WEBAPI_URL, CELLS_TABLE_PATH + "cell_"+ current_cell_id_val, None, None,
-                                          None,
-                                          "SET "+ITEM_PREFIX+"count=if_not_exists("+ITEM_PREFIX+"count,0)+1;",
-                                          None)
 
-            if res.status_code not in (200,204):
-                context.logger.error ("Error during increment of count in cells table. Error code is "+str(res.status_code))
-                return context.Response(body='Internal error during ingestion',content_type='text/plain',status_code=500)
+            count_attribute = item_prefix+'count'
 
+            # Increase the count on the currnet cell
+            res = _webapi_updateitem(WEBAPI_URL,
+                                     CELLS_TABLE_PATH + "cell_" + current_cell_id_val,
+                                     f'SET {count_attribute}=if_not_exists({count_attribute},0)+1;')
 
-            # Decrease the count for the driver's previous cell
-            res = ngx_update_expression_request(s,WEBAPI_URL, CELLS_TABLE_PATH + "cell_" + previous_cell_id_val, None, None,None,
-                                          "SET "+ITEM_PREFIX+"count="+ITEM_PREFIX+"count-1;",
-                                          None)
+            if res.status_code != requests.codes.no_content:
+                context.logger.error(f'Error during increment of count in cells table. Error code is {res.status_code}')
+                return context.Response(body='Internal error during ingestion', content_type='text/plain', status_code=500)
 
-            # if res.status_code not in (200,204):
-            #     context.logger.error ("Error during decrement of count in cells table. Error code is "+str(res.status_code))
-            #     return context.Response(body='Internal error during ingestion',content_type='text/plain',status_code=500)
+            # Decrease the count on the previous cell
+            if int(previous_cell_id_val) > 0:
+                res = _webapi_updateitem(WEBAPI_URL,
+                                         CELLS_TABLE_PATH + "cell_" + previous_cell_id_val,
+                                         f'SET {count_attribute}={count_attribute}-1;')
 
-    return context.Response(body='Ingestion completed successfully',
-                            headers={},
-                            content_type='text/plain',
-                            status_code=200)
+            if res.status_code != requests.codes.no_content:
+                    context.logger.error(f'Error during decrement of count in cells table. Error code is {res.status_code}')
+                    return context.Response(body='Internal error during ingestion', content_type='text/plain', status_code=500)
+
+    return context.Response(status_code=requests.codes.no_content)
+
 
 #
-# Construct and send a GetItem NoSQL Web API request
+# Construct and send GetItem NoSQL Web API reuest
 #
-def ngx_get_item_request(
-        s,base_url, path_in_url, table_name=None, key=None, exp_attrs=None, expected_result=requests.codes.ok):
-    url = os.path.join(base_url, path_in_url)
+def _webapi_getitem(base_url, path_in_url, exp_attrs):
 
-    # Build a JSON request body
+    url = base_url + path_in_url
+
+    #
+    # Construct the request json
+    #
     request_json = {}
-
-    if table_name is not None:
-        request_json["TableName"] = table_name
 
     request_json["AttributesToGet"] = ""
 
@@ -125,53 +144,43 @@ def ngx_get_item_request(
         request_json["AttributesToGet"] += attr_name
 
     payload = json.dumps(request_json)
-    
-    if WEBAPI_CRED is None:
-        headers = {V3IO_HEADER_FUNCTION: 'GetItem'}
-    else:
-        headers = {V3IO_HEADER_FUNCTION: 'GetItem',"Authorization": str(WEBAPI_CRED)}
-    
-    # Send the request
-    res = s.put(url, data=payload, headers=headers)
 
-    assert res.status_code == expected_result
-    if expected_result != requests.codes.ok:
+    headers = {V3IO_HEADER_FUNCTION: "GetItem"}
+
+    if WEBAPI_CRED is not None:
+        headers["Authorization"] = str(WEBAPI_CRED)
+
+    # send the request
+    res = requests.put(url, data=payload, headers=headers)
+
+    if res.status_code != requests.codes.ok:
         return
 
     response_json = json.loads(res.content)
     return response_json
 
-#
-# Construct and send an UpdateItem NoSQL Web API request
-#
-def ngx_update_expression_request(
-        s,base_url, path_in_url, table_name=None, key=None, mode=None, update_expr=None, text_filter=None, type="UpdateItem",
-        expected_result=requests.codes.no_content):
-    url = os.path.join(base_url, path_in_url)
 
-    # Build a JSON request body
+#
+# Construct and send UpdateItem NoSQL Web API reuest
+#
+def _webapi_updateitem(base_url, path_in_url, update_expr):
+
+    url = base_url + path_in_url
+
+    #
+    # Construct the request json
+    #
     request_json = {}
 
-    if table_name is not None:
-        request_json["TableName"] = table_name
-
-    if mode is not None:
-        request_json["UpdateMode"] = mode
-
-    if update_expr is not None:
-        request_json["UpdateExpression"] = update_expr
-
-    if text_filter is not None:
-        request_json["ConditionExpression"] = text_filter
+    request_json["UpdateExpression"] = update_expr
 
     payload = json.dumps(request_json)
-    
-    if WEBAPI_CRED is None:
-       headers = {V3IO_HEADER_FUNCTION: type }
-    else:
-       headers = {V3IO_HEADER_FUNCTION: type ,"Authorization": str(WEBAPI_CRED)}
 
-    # Send the request
-    res = s.put(url, data=payload, headers=headers)
-    #assert res.status_code == expected_result
+    headers = {V3IO_HEADER_FUNCTION: "UpdateItem"}
+
+    if WEBAPI_CRED is not None:
+        headers["Authorization"] = str(WEBAPI_CRED)
+
+    # send the request
+    res = requests.put(url, data=payload, headers=headers)
     return res
