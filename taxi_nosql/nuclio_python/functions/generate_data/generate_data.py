5c6c48ce49f25c64f769324ee970002fd62a60d8
Sharon Lifshitz <sharonl@iguaz.io>
2018-07-24 08:31:41 +0300

    [IG-7915 taxi_nosql DOC review] generate-data.py comments review

--- taxi_nosql/nuclio_python/functions/generate_data/generate_data.py
+++ taxi_nosql/nuclio_python/functions/generate_data/generate_data.py
@@ -24,25 +24,27 @@ drivers_weighted_locations = {'downtown_london': 4, 'westminster': 3}
 passengers_weighted_locations = \
     {'heathrow': 4, 'heathrow_parking': 3, 'gatwick': 4}
 
-# Get IP and Port of the ingestion function from Nuclio environment variables
+# Get the ingestion-function URL from the Nuclio function environment variables
 ingest_url = os.getenv("INGEST_URL")
 
-# Number of drivers and passengers to ingest
+# Set the number of drivers and passengers to ingest
 num_drivers_to_ingest = 1000
 num_passengers_to_ingest = 500
 
-# Max ID for drivers and passengers, defines the total number in the system
+# Set the maximum valid driver and passenger ID values
 max_driver_id = 5000
 max_passenger_id = 5000
 
 
+# Function handler - generate random drivers, passengers, and locations data
+# and send it to the configured ingestion URL (INGEST_URL)
 def handler(context, event):
 
-    # ingest drivers
+    # Ingest current driver locations
     _ingest_locations(context, ingest_url, num_drivers_to_ingest,
                       max_driver_id, 'driver', drivers_weighted_locations)
 
-    # ingest passengers
+    # Ingest current passenger locations
     _ingest_locations(context, ingest_url, num_passengers_to_ingest,
                       max_passenger_id, 'passenger',
                       passengers_weighted_locations)
@@ -50,16 +52,21 @@ def handler(context, event):
     return context.Response(status_code=204)
 
 
+# Ingest driver and passenger locations information
 def _ingest_locations(context, ingest_url, num_records, max_record_id,
                       record_type, weighted_locations):
 
-    # generate random location for 1000 drivers and send for ingestion
+    # Get random driver/passenger locations and send the data for ingestion
     for x in range(1, num_records):
 
-        # get a random locations
+        # Get a weighted random location
         random_location = _get_random_location(_weighted_keys(locations,
                                                weighted_locations))
 
+        # Build a JSON request body with the record type (driver/passenger),
+        # the respective ID, and the GPS coordinates of the current driver or
+        # passenger location. For demo purposes, the location coordinates are
+        # generated as a random offset of the current random weighted location.
         request = {
             'RecordType': record_type,
             'ID': random.randint(1, max_record_id),
@@ -68,6 +75,8 @@ def _ingest_locations(context, ingest_url, num_records, max_record_id,
             'Latitude': _get_random_offset(locations[random_location]['lat'])
         }
 
+        # Ingest the location data by sending a PUT Object web-API request to
+        # the configured ingestion URL
         response = requests.put(ingest_url, data=json.dumps(request))
 
         if response.status_code != requests.codes.ok:
@@ -77,7 +86,7 @@ def _ingest_locations(context, ingest_url, num_records, max_record_id,
             return context.Response(body={'error': message}, status_code=500)
 
 
-# get a random location from the available locations
+# Get a random location from the available locations
 def _get_random_location(locations):
 
     random_location_name = random.choice(locations)
@@ -85,20 +94,18 @@ def _get_random_location(locations):
     return random_location_name
 
 
-# move to a close offset of the exact point - for demo purposes only
+# Generate a close random offset of the given location - for demo purposes only
 def _get_random_offset(location):
 
     rnd_radius = random.randint(0, 8)
-
     rnd_offset = random.randint(-rnd_radius, rnd_radius)
-
     new_location = Decimal(location) + (Decimal(rnd_offset) / Decimal(300))
 
     return str(new_location)
 
 
-# given dict "d", returns the keys weighted by weight "weights"
-# For example:
+# Get weighted keys: given dictionary "d", return the dictionary keys weighted
+# by weight "weights". For example:
 #  input: d = {'a': 1, 'b': 2, 'c': 3}, weights = {'b': 3, 'c': 2}
 #  output: ['a', 'b', 'b', 'b', 'c', 'c']
 def _weighted_keys(d, weights):
