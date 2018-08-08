ff2c04e98fc817dd5ae8bbcc0cb98fc3c8e6f57a
Eran Duchan <pavius@gmail.com>
2018-08-09 00:47:10 +0300

    added query skeleton

--- netops_demo/golang/src/github.com/v3io/demos/main.go
+++ netops_demo/golang/src/github.com/v3io/demos/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	
 	"github.com/v3io/demos/functions/ingest"
+	"github.com/v3io/demos/functions/query"
 
 	"github.com/nuclio/nuclio-sdk-go"
 )
@@ -12,11 +13,17 @@ func Ingest(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 	return ingest.Ingest(context, event)
 }
 
+func Query(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
+	return query.Query(context, event)
+}
+
 // InitContext runs only once when the function runtime starts
 func InitContext(context *nuclio.Context) error {
 	switch os.Getenv("NUCLIO_HANDLER") {
 	case "main:Ingest":
 		return ingest.InitContext(context)
+	case "main:Query":
+		return query.InitContext(context)
 	}
 
 	return nil
